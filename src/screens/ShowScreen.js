167cd244846ffc0f16f326962f08368d0ca5c953
Stephen Moore <judo2000@comcast.net>
2023-08-05 16:10:27 -0600

    Update ShowScreen to show blog post content and add edit pencile in headerRight

--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { Context } from '../context/BlogContext';
+import { EvilIcons } from '@expo/vector-icons';
 
 const ShowScreen = ({ navigation }) => {
   const id = navigation.getParam('id');
@@ -11,10 +12,22 @@ const ShowScreen = ({ navigation }) => {
   return (
     <View>
       <Text>{blogPost.title}</Text>
+      <Text>{blogPost.content}</Text>
     </View>
   );
 };
 
+ShowScreen.navigationOptions = ({ navigation }) => {
+  return {
+    headerRight: () => (
+      <TouchableOpacity
+        onPress={() => navigation.navigate('Edit', { id: title.id })}
+      >
+        <EvilIcons name="pencil" size={35} />
+      </TouchableOpacity>
+    ),
+  };
+};
 const styles = StyleSheet.create({});
 
 export default ShowScreen;
