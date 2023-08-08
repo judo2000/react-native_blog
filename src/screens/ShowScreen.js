d2ecdc347e52d620ededd9212103f5676191c925
Stephen Moore <judo2000@comcast.net>
2023-08-08 14:14:43 -0600

    Create BlogPostForm as resusable component for Create and edit blog, and update Create Screen

--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -4,11 +4,11 @@ import { Context } from '../context/BlogContext';
 import { EvilIcons } from '@expo/vector-icons';
 
 const ShowScreen = ({ navigation }) => {
-  const id = navigation.getParam('id');
-
   const { state } = useContext(Context);
 
-  const blogPost = state.find((blogPost) => blogPost.id === id);
+  const blogPost = state.find(
+    (blogPost) => blogPost.id === navigation.getParam('id')
+  );
   return (
     <View>
       <Text>{blogPost.title}</Text>
@@ -21,7 +21,9 @@ ShowScreen.navigationOptions = ({ navigation }) => {
   return {
     headerRight: () => (
       <TouchableOpacity
-        onPress={() => navigation.navigate('Edit', { id: title.id })}
+        onPress={() =>
+          navigation.navigate('Edit', { id: navigation.getParam('id') })
+        }
       >
         <EvilIcons name="pencil" size={35} />
       </TouchableOpacity>
